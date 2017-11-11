8a5c0ff3ae343415c7aac00283096f040798371a
Kaj Ström <kaj.strom@kapsi.fi>
2017-11-11 18:49:55 +0200

    IngredientTypesTest now decodes JSON to test more reliably

--- src/test/java/fi/kajstrom/kitcheninventory/functional/IngredientTypesTest.java
+++ src/test/java/fi/kajstrom/kitcheninventory/functional/IngredientTypesTest.java
@@ -1,14 +1,15 @@
 package fi.kajstrom.kitcheninventory.functional;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.impl.client.HttpClientBuilder;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 import java.io.IOException;
+import java.util.*;
 
 public class IngredientTypesTest extends FunctionalTest {
     @Test
@@ -20,6 +21,13 @@ public class IngredientTypesTest extends FunctionalTest {
         HttpResponse response = execute(request);
 
         assertEquals(200, response.getStatusLine().getStatusCode());
-        assertTrue(responseBodyAsString(response).contains("Sugar"));
+
+        ObjectMapper mapper = new ObjectMapper();
+        HashMap[] array = mapper.readValue(responseBodyAsString(response), HashMap[].class);
+        List<Map<String, Object>> lm = Arrays.asList(array);
+        Optional<Map<String, Object>> sugar =
+                lm.stream().filter((el) -> (Integer)el.get("id") == 1).findFirst();
+
+        assertEquals("Sugar", sugar.get().get("name"));
     }
 }
