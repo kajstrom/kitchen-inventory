f3b062dcde70180c73220f2738638b55c09f59b3
Kaj Ström <kaj.strom@kapsi.fi>
2017-11-11 16:30:01 +0200

    WIP IngredientType with naive test

---
+++ src/main/java/fi/kajstrom/kitcheninventory/Adapters/REST/IngredientTypeAdapter.java
@@ -0,0 +1,7 @@
+package fi.kajstrom.kitcheninventory.Adapters.REST;
+
+/**
+ * Created by Kaitsu on 11.11.2017.
+ */
+public class IngredientTypeAdapter {
+}
---
+++ src/test/java/fi/kajstrom/kitcheninventory/functional/IngredientTypesTest.java
@@ -0,0 +1,7 @@
+package fi.kajstrom.kitcheninventory.functional;
+
+/**
+ * Created by Kaitsu on 11.11.2017.
+ */
+public class IngredientTypesTest {
+}
