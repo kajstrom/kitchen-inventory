070aa440c2c4f98f916f75eaaa1b45730bb8c1ad
Kaj Ström <kaj.strom@kapsi.fi>
2017-11-14 19:19:48 +0200

    Adds database table ingredients

--- src/main/resources/database/schema.sql
+++ src/main/resources/database/schema.sql
@@ -5,3 +5,15 @@ CREATE TABLE ingredient_types(
     name VARCHAR(200) NOT NULL,
     quantity_unit VARCHAR(100) NOT NULL
 ) ENGINE InnoDb;
+
+DROP TABLE IF EXISTS ingredients;
+
+CREATE TABLE ingredients(
+    ingredient_id INT PRIMARY KEY AUTO_INCREMENT,
+    ingredient_type_id INT,
+    quantity DOUBLE(7, 2) UNSIGNED NOT NULL,
+    CONSTRAINT fk_ingredient_type
+        FOREIGN KEY (ingredient_type_id) REFERENCES ingredient_types(ingredient_type_id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+) ENGINE InnoDb;
