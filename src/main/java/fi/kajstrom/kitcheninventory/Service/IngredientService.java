f6111ef68ec5ead8868c9e70e02940199155a546
Kaj Ström <kaj.strom@kapsi.fi>
2017-11-18 19:36:37 +0200

    Adds Mockito

---
+++ src/main/java/fi/kajstrom/kitcheninventory/Service/IngredientService.java
@@ -0,0 +1,7 @@
+package fi.kajstrom.kitcheninventory.Service;
+
+/**
+ * Created by Kaitsu on 18.11.2017.
+ */
+public class IngredientService {
+}
---
+++ src/test/java/fi/kajstrom/kitcheninventory/integration/Service/IngredientServiceTest.java
@@ -0,0 +1,7 @@
+package fi.kajstrom.kitcheninventory.integration.Service;
+
+/**
+ * Created by Kaitsu on 18.11.2017.
+ */
+public class IngredientServiceTest {
+}
